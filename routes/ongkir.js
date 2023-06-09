b3460ba8d0016992d52e503fa4e5075e9a10d1e8
Naufaldwar <naufaldwiariananto@gmail.com>
2023-06-09 23:15:10 +0700

    fixing post cost ongkir

--- routes/ongkir.js
+++ routes/ongkir.js
@@ -17,23 +17,27 @@ router.get("/city", async (req, res) => {
 
 router.post("/cost", async (req, res) => {
   try {
-    const { origin, destination, weight, courier } = req.body;
-    const apiKey = "c989dc4c3d61abcf05738ce739f02750";
+    const { key, origin, destination, weight, courier } = req.body;
 
     // Lakukan logika pemrosesan atau panggil fungsi untuk menghitung ongkos kirim menggunakan parameter yang diterima
 
     // Contoh penggunaan Axios untuk mengirim permintaan POST ke RajaOngkir API
     const response = await axios.post(
-      `https://api.rajaongkir.com/starter/cost?key=${apiKey}`,
+      "https://api.rajaongkir.com/starter/cost",
       {
         origin,
         destination,
         weight,
         courier,
+      },
+      {
+        headers: {
+          key,
+        },
       }
     );
 
-    res.json(response.data);
+    res.json(response.data.rajaongkir.results);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
