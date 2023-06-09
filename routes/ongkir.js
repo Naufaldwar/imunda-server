a33fa11bdf4f1ed1cc96036612cdecd22e226a27
Naufaldwar <naufaldwiariananto@gmail.com>
2023-06-09 23:02:45 +0700

    add post route

--- routes/ongkir.js
+++ routes/ongkir.js
@@ -15,4 +15,28 @@ router.get("/city", async (req, res) => {
   }
 });
 
+router.post("/cost", async (req, res) => {
+  try {
+    const { origin, destination, weight, courier } = req.body;
+    const apiKey = "c989dc4c3d61abcf05738ce739f02750";
+
+    // Lakukan logika pemrosesan atau panggil fungsi untuk menghitung ongkos kirim menggunakan parameter yang diterima
+
+    // Contoh penggunaan Axios untuk mengirim permintaan POST ke RajaOngkir API
+    const response = await axios.post(
+      `https://api.rajaongkir.com/starter/cost?key=${apiKey}`,
+      {
+        origin,
+        destination,
+        weight,
+        courier,
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
