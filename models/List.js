e3e7e9d7770f7812f9f3d5b5ade917ea5dc1fab6
Naufaldwar <naufaldwiariananto@gmail.com>
2023-06-10 02:25:17 +0700

    change model

--- models/List.js
+++ models/List.js
@@ -1,16 +1,44 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const ListSchema = new Schema({
-    description: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  payment: {
+    type: String,
+    required: true,
+  },
+  name_product: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  variant: {
+    type: String,
+    required: true,
+  },
+  //   date: {
+  //     type: Date,
+  //     default: Date.now,
+  //   },
 });
 
-const List = model('list', ListSchema);
+const List = model("list", ListSchema);
 
 module.exports = List;
