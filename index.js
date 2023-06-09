e7919083196fc8a50e3d8970bb340601b6fc04aa
Naufaldwar <naufaldwiariananto@gmail.com>
2023-06-09 22:01:38 +0700

    add cors rajaongkir

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
-
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
 const mongoose = require("mongoose");
 
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://api.rajaongkir.com",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "/starter", // Optional: If the API path starts with '/starter'
+    },
+  })
+);
 // Connect to MongoDB
 mongoose.connect(
   "mongodb://naufaldwiariananto:suqc15NzJaCG9Lu2@ac-wxu6n3x-shard-00-00.ydcdonf.mongodb.net:27017,ac-wxu6n3x-shard-00-01.ydcdonf.mongodb.net:27017,ac-wxu6n3x-shard-00-02.ydcdonf.mongodb.net:27017/?ssl=true&replicaSet=atlas-7qlv4q-shard-0&authSource=admin&retryWrites=true&w=majority",
