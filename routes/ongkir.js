5dfbeaf605a99bc1e4041840f5dc2cd3cb47bbfe
naufaldwar <naufaldwiariananto@gmail.com>
2023-06-11 16:23:23 +0700

    change key

--- routes/ongkir.js
+++ routes/ongkir.js
@@ -4,7 +4,8 @@ const axios = require("axios");
 
 router.get("/city", async (req, res) => {
   try {
-    const apiKey = "ce8b738b8260f386ae528dcff159a7b0";
+    // 520f9b99617a9af001a9b4a2d4c31717 , ce8b738b8260f386ae528dcff159a7b0
+    const apiKey = "520f9b99617a9af001a9b4a2d4c31717";
     const response = await axios.get(
       `https://api.rajaongkir.com/starter/city?key=${apiKey}`
     );
